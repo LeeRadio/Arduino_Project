164f8d980a10f2dc147aa88865f0df080d735c4b
LeeRadio <acorn0987@naver.com>
2022-10-23 20:27:28 +0900

    update

--- Welcome To Home/home/home.ino
+++ Welcome To Home/home/home.ino
@@ -1,9 +1,20 @@
 // 핀 정의
 #define GREEN_LED 5
-#define YELLO_LED 6
+#define BLUE_LED 6
+#define RED_LED 7
+
+int Br = 255;
+int gBr = 0;
+int bBr = 0;
+int rBr = 0;
+
+int fadeSpeed = 5;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode (GREEN_LED, OUTPUT);
+  pinMode (BLUE_LED, OUTPUT);
+  pinMode (RED_LED, OUTPUT);
 
 }
 
