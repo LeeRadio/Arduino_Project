30da21cbeed514455b471afd859962715d1587c5
LeeRadio <acorn0987@naver.com>
2022-10-23 20:24:09 +0900

    기본 코드만

---
+++ Welcome To Home/home/home.ino
@@ -0,0 +1,13 @@
+// 핀 정의
+#define GREEN_LED 5
+#define YELLO_LED 6
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
